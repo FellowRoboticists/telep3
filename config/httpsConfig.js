2a5353617ab2b7ce137e3ff660fe4e3c33e56788
Naive Roboticist <naiveroboticist@gmail.com>
2016-08-19 15:22:32 -0400

    Modified https configuration for new location.

--- config/httpsConfig.js
+++ config/httpsConfig.js
@@ -3,8 +3,10 @@
 module.exports = (function () {
   const fs = require('fs')
 
-  const HTTPS_KEY = '/etc/ssl/private/telep3-key.pem'
-  const HTTPS_CERT = '/etc/ssl/certs/telep3-cert.pem'
+  const HTTPS_KEY = '/etc/telep/private/telep3-key.pem'
+  const HTTPS_CERT = '/etc/telep/certs/telep3-cert.pem'
+  // const HTTPS_KEY = '/etc/ssl/private/telep3-key.pem'
+  // const HTTPS_CERT = '/etc/ssl/certs/telep3-cert.pem'
   const DEFAULT_PORT = '8443'
 
   var mod = {
