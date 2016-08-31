28968db8a92bd73bbda18a5da242e4a6de12b3a0
Naive Roboticist <naiveroboticist@gmail.com>
2016-08-31 16:08:35 -0600

    Got a start on the deploy script.

---
+++ deploy/deploy.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#
+# Use this script to package up the telep3 application and
+# deploy it to the server machine.
+
+DEPLOY_DIR=$(cd $(dirname $0) && pwd)
+
+. ${DEPLOY_DIR}/deploy-support-tools/git-support.sh
+. ${DEPLOY_DIR}/deploy-support-tools/package-support.sh
+. ${DEPLOY_DIR}/deploy-support-tools/remote-support.sh
+. ${DEPLOY_DIR}/deploy-support-tools/node-support.sh
+
+verbose_level=3
+BRANCH=master
+REPO_DIR=${DEPLOY_DIR}/repo
+SOURCE_DIR=${DEPLOY_DIR}/source
+PACKAGE_DIR=${DEPLOY_DIR}/packages
+BASE_DIR=/var/www/telep3
+RELEASES_DIR=${BASE_DIR}/releases
+CURRENT_DIR=${BASE_DIR}/current
+REPOSITORY=git@github.com:FellowRoboticists/telep3.git
+NAME=telep3
+NODE_VERSION=v6.1.0
+MACHINE=do-development
+
+usage() {
+  cat <<EOF
+  usage $0 options
+
+  This script packages up telep3 and deploys it to the server.
+
+  OPTIONS:
+    -b branch  the git branch to deploy (default 'master')
+    -h         Display this help message
+    -m machine the machine to deploy to (defalt 'do-development')
+    -n version the version of node to use (default 'v6.1.0')
+    -v         Verbose output
+EOF
+}
+
+# Parse the command line
+while getopts :b:hm:n:v OPTION
+do
+  case $OPTION in
+    b)
+      BRANCH=${OPTARG}
+      ;;
+    h)
+      usage
+      exit 1
+      ;;
+    m)
+      MACHINE=${OPTARG}
+      ;;
+    n)
+      NODE_VERSION=${OPTARG}
+      ;;
+    v)
+      verbose_level=4
+      ;;
+    ?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+shift "$(($OPTIND-1))"
+
+ACTION=${1:-create}
+
+# Main program
+debug "BRANCH       = ${BRANCH}"
+debug "ACTION       = ${ACTION}"
+debug "MACHINE      = ${MACHINE}"
+debug "NODE_VERSION = ${NODE_VERSION}"
+
+packageName=${NAME}-${BRANCH}.tar.bz2
+
+createPackage() {
+  local packageName=$1
+
+  prepareSource ${REPO_DIR} ${SOURCE_DIR} ${REPOSITORY} ${NAME} ${BRANCH}
+
+  prepareNodeModules ${SOURCE_DIR} ${NAME} ${NODE_VERSION}
+
+  createReleasePackage ${SOURCE_DIR} ${NAME} ${PACKAGE_DIR} ${packageName}
+}
+
+if [ "${ACTION}" == clear ]
+then
+  rm -f ${PACKAGE_DIR}/${packageName}
+fi
+
+if [ "${ACTION}" == create ]
+then
+  createPackage ${packageName}
+fi
